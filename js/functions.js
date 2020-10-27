0a41d3e4b39bff09c089667bdb9a57a24bcdacdf
Hôih My <my.hoih@student.passerellesnumeriques.org>
2020-10-27 14:47:58 +0700

    Correct the time of love

--- js/functions.js
+++ js/functions.js
@@ -35,32 +35,63 @@ $(window).resize(function() {
 
 function timeElapse(date) {
 	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var miliseconds = (Date.parse(current) - Date.parse(date)) / 1000;
 
-	// var years = Math.floor(seconds / (3600 * 24 * 30.44 * 12));
+	// // var years = Math.floor(seconds / (3600 * 24 * 30.44 * 12));
 
-	// seconds = seconds % (3600 * 24 * 30.44 * 12);
-	var days = Math.floor(seconds / (3600 * 24));
+	// // seconds = seconds % (3600 * 24 * 30.44 * 12);
+	// var days = Math.floor(seconds / (3600 * 24));
 	
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
+	// seconds = seconds % (3600 * 24);
+	// var hours = Math.floor(seconds / 3600);
+	// if (hours < 10) {
+	// 	hours = "0" + hours;
+	// }
+
+	// seconds = seconds % 3600;
+	// var minutes = Math.floor(seconds / 60);
+	// if (minutes < 10) {
+	// 	minutes = "0" + minutes;
+	// }
+
+	// seconds = seconds % 60;
+	// if (seconds < 10) {
+	// 	seconds = "0" + seconds;
+	// }
+
+	var years = Math.floor(miliseconds / 31536000);
+	miliseconds -= years * 31536000;
+
+	var months = Math.floor(miliseconds / 2592000);
+	miliseconds -= months * 2592000;
+
+	var weeks = Math.floor(miliseconds / 604800);
+	miliseconds -= weeks * 604800;
+
+	var days = Math.floor(miliseconds / 86400);
+	miliseconds -= days * 86400;
+
+	var hours = Math.floor(miliseconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
+	miliseconds -= hours * 3600;
+	var minutes = Math.floor(miliseconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
 
-	seconds = seconds % 60;
+	miliseconds -= minutes * 60;
+	var seconds = Math.floor(miliseconds / 1);
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
 
 	var result = "<span class=\"digit\">" 
-	// + years + "</span> năm <span class=\"digit\">"
+	+ years + "</span> năm <span class=\"digit\">"
+	+ months + "</span> tháng <span class=\"digit\">"
+	+ weeks + "</span> tuần <span class=\"digit\">"
 	+ days + "</span> ngày <span class=\"digit\">" 
 	+ hours + "</span> giờ <span class=\"digit\">" 
 	+ minutes + "</span> phút <span class=\"digit\">" 
